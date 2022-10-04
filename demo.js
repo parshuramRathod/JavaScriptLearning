39d92ab52527f283e6a33dac482ee8eec138de9d
Parshuram <parshuramrathod9540@gmail.com>
2022-10-04 08:44:51 +0530

    started js journey
    started js journey

---
+++ demo.js
@@ -0,0 +1,7 @@
+// This is my first programm
+// Now i know the two ways to run programm
+console.log("Hello");
+console.log("My name is Parshuram Rathod");
+console.info("This is info");//info means information
+console.warn("I am not that bad guy");// warn means warning
+console.error("Opps failed to execute")//in output shows red color
