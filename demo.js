99fc530e0aaf70ce1e6d5245b99e890e01f92286
Parshuram <parshuramrathod9540@gmail.com>
2022-10-04 17:58:45 +0530

    first program execution in js
    first program execution in js

--- demo.js
+++ demo.js
@@ -1,7 +1,10 @@
 // This is my first programm
 // Now i know the two ways to run programm
 console.log("Hello");
-console.log("My name is Parshuram Rathod");
-console.info("This is info");//info means information
-console.warn("I am not that bad guy");// warn means warning
-console.error("Opps failed to execute")//in output shows red color
+console.log("My name is Parshuram Tukaram Rathod");
+console.log("Hobbies:1.Cricket 2.Adventure 3.cooking")
+console.info("Strenth:1.Quick learner 2. self motivated and flexible 3. Hard worker ");//info means information
+console.warn("Weakness: 1.Impatience 2.Asking for help 3.Nervousness");// warn means warning
+console.log(30+50);
+console.log(49*479);
+//console.error("Opps failed to execute")//in output shows red color
