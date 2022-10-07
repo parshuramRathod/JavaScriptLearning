482b7febad2cc44408cc64b7c145669c7c818908
Parshuram <parshuramrathod9540@gmail.com>
2022-10-07 06:48:21 +0530

    some value changed
    some value changed

--- 02_assignment_02.js
+++ 02_assignment_02.js
@@ -1,6 +1,6 @@
 var myName = "Parshuram Rathod";
 console.log(myName);
-var email_id = "parshuramrathod9540@gmail.com"
+var email_id = "parshuramrathod9540@gmail.com";
 console.log(email_id);
 var mobile_no = 8177852570;
 console.log(mobile_no);
@@ -13,12 +13,13 @@ a=num_1;
 num_1=num_2;
 var num_2=a;
 console.log("after",num_1,num_2);
-var num_1 = 500, num_2 = 100, num_3 = 1050;
+var num_1 = 50, num_2 = 1000, num_3 = 150;
 console.log(num_1,num_2,num_3);
 b=num_1;
 num_1=num_2;
-var num_2=b;
-c=num_2;
+// // var num_2=b;
+// c=num_2;
 num_2=num_3;
-var num_3=c;
+num_3=b;
+// var num_3=c;
 console.log(num_1,num_2,num_3);
