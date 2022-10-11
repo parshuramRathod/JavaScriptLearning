8980051f22e260d4f1d5a948a538e4becc2f47e1
Parshuram <parshuramrathod9540@gmail.com>
2022-10-11 12:14:00 +0530

    learned more properties of string and assignment done.
    learned more properties of string and assignment done.

---
+++ 04_assignment_01.js
@@ -0,0 +1,10 @@
+console.log("================1=================");
+console.log(`'My dream company is "Google"'`);
+console.log("================2=================");
+var hobby_1 = "Cricket";
+var hobby_2 = "Adventure"
+var hobby_3 = "Cooking"
+console.log(`Hobby 1 : ${hobby_1}`);
+console.log(`Hobby 2 : ${hobby_2}`);
+console.log(`Hobby 3 : ${hobby_3}`);
+console.log(`After concat : ${hobby_1.concat(" ", hobby_2, " ", hobby_3)}`);
